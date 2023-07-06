d164d444e4e7455e6b46ff792b9eea753cba3364
SyRachid <syrachidsanogo78@gmail.com>
2023-07-06 17:04:07 +0000

    commit for requirements

--- test.py
+++ test.py
@@ -4,81 +4,49 @@ def test_add():
     input_1 = 12
     input_2 = 13
     expected_outpout = 25
-    try:
-        assert f.addition(input_1,input_2) == expected_outpout
-        print('addition test case 1 passed')
-    except AssertionError:
-        print("addition test case 1 failed")
+    assert f.addition(input_1,input_2) == expected_outpout
+     
     input_1 = -5
     input_2 = 10
     expected_outpout = 5
-    try:
-        assert f.addition(input_1,input_2) == expected_outpout
-        print('addition test case 2 passed')
-    except AssertionError:
-        print("addition test case 2 failed")
+   
+    assert f.addition(input_1,input_2) == expected_outpout
+   
     input_1 = -5
     input_2 = -5
     expected_outpout = -10
-    try:
-        assert f.addition(input_1,input_2) == expected_outpout
-        print('addition test case 2 passed')
-    except AssertionError:
-        print("addition test case 2 failed")
+    assert f.addition(input_1,input_2) == expected_outpout
     input_1 = -5
     input_2 = 5.5
     expected_outpout = 0.5
-    try:
-        assert f.addition(input_1,input_2) == expected_outpout
-        print('addition test case 4 passed')
-    except AssertionError:
-        print("addition test case 4 failed")
+    assert f.addition(input_1,input_2) == expected_outpout
+
 #test de la fonction difference
 def test_sous():
     input_1 = 12
     input_2 = 13
     expected_outpout = -1
-    try:
-        assert f.soustraction(input_1,input_2) == expected_outpout
-        print('soustraction test case 1 passed')
-    except AssertionError:
-        print("soustraction test case 1 failed")
+    assert f.soustraction(input_1,input_2) == expected_outpout
     input_1 = -5
     input_2 = 10
     expected_outpout = -15
-    try:
-        assert f.soustraction(input_1,input_2) == expected_outpout
-        print('soustraction test case 2 passed')
-    except AssertionError:
-        print("soustraction test case 2 failed")
+    assert f.soustraction(input_1,input_2) == expected_outpout
     input_1 = -5
     input_2 = -5
     expected_outpout = 0
-    try:
-        assert f.soustraction(input_1,input_2) == expected_outpout
-        print('soustraction test case 2 passed')
-    except AssertionError:
-        print("soustraction test case 2 failed")
+    assert f.soustraction(input_1,input_2) == expected_outpout
     input_1 = -5
     input_2 = 5.5
     expected_outpout = -10.5
-    try:
-        assert f.soustraction(input_1,input_2) == expected_outpout
-        print('soustraction test case 4 passed')
-    except AssertionError:
-        print("soustraction test case 4 failed")
-
-
+    assert f.soustraction(input_1,input_2) == expected_outpout
+   
+"""
 #test de la multiplication
 def test_mul():
     input_1 = 12
     input_2 = 11
     expected_outpout = 132
-    try:
-        assert f.multiplication(input_1,input_2) == expected_outpout
-        print('multiplication test case 1 passed')
-    except AssertionError:
-        print("multiplication test case 1 failed")
+    assert f.multiplication(input_1,input_2) == expected_outpout
     input_1 = -5
     input_2 = 10
     expected_outpout = -50
@@ -154,3 +122,4 @@ print("")
 test_add()
 print("")
 test_sous()
+"""
