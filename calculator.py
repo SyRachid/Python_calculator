cac17702580cb63aa2365facdd80ef216d729d34
SyRachid <syrachidsanogo78@gmail.com>
2023-07-06 14:29:07 +0000

    commit to fix print problem in console

--- calculator.py
+++ calculator.py
@@ -3,8 +3,12 @@ import checker as c
 i = 1
 while (i == 1):
     premier,second=c.checkEntree()
-    print("tapez 1 pour addition\n tapez 2 pour soustraction\n tapez 3 pour multiplication\n tapez 4 pour division\n")
+    print("\n")
+    print("tapez 1 pour addition\ntapez 2 pour soustraction\ntapez 3 pour multiplication\ntapez 4 pour division\n")
     operation = float(input("operation : "))
     result = c.checkOp(operation,premier,second)
+    print("\n")
     print("le résultat est "+str(result))
+    print("\n")
     i = float(input("tapez 1 pour effectuer une autre operation et tout autre touche pour quitter: "))
+    print("\n")
